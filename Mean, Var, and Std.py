7a13c0fdabf3e1bddd80ec07590a1941ae3310ca
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-04-19 20:36:37 +0200

    mean,var,std

--- Mean, Var, and Std.py
+++ Mean, Var, and Std.py
@@ -2,5 +2,6 @@ import numpy as np
 
 n,m = map(int,input().strip().split(' '))
 arr = np.array([input().strip().split(' ') for m in range(n)],dtype=int)
-minimum = np.min(arr,axis=1)
-print(np.max(minimum))
+print(np.mean(arr,axis=1))
+print(np.var(arr,axis=0))
+print(round(np.std(arr,axis=None),11))
